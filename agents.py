ed25b1822aff136396c76e52981f18dd5c06d59d
AOUTIR Mouad <135459740+moaoutir@users.noreply.github.com>
2024-11-22 21:06:30 +0100

    Update agents.py

--- agents.py
+++ agents.py
@@ -38,7 +38,7 @@ class RecipeAgents:
         groq_api_key = os.getenv("GROQ_API_KEY")
         model_name = os.getenv("GROG_MODEL_NAME")
 
-        if not api_key or not model_name:
+        if not groq_api_key or not model_name:
             raise EnvironmentError("Missing OPENAI_API_KEY or OPENAI_MODEL_NAME in environment variables.")
 
         self.llm = LLM(
